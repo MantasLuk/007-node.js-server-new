b05406e465bdffe1fcbf297987c290d317de4608
Mantas Luk <mantasluk7@gmail.com>
2022-01-28 20:39:47 +0200

    Read folder

--- components/servicesSection.js
+++ components/servicesSection.js
@@ -1,6 +1,10 @@
-function servicesSection() {
+const folder = require("../lib/folder");
+
+async function  servicesSection() {
 
     //perskaitome kokie failai yra: /data/services
+    const files = await folder.read('data/services')
+    console.log(files);
     //gauname sarasa paslaugu json failu
     //visus JSON failus issiparsinti, konvertuoji i JS objekta
     // atfiltruoti tik validzias ir aktyvias paslaugas, t.y. ju objektus
@@ -11,30 +15,30 @@ function servicesSection() {
 
 
 
-    
+
     return `<section class="container bg-gradient services">
-    <div class="row">
-        <h2>Services</h2>
-        <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back into the development of the asset through its charitable foundation.</p>
-    </div>
-    <div class="row services-list">
-        <div class="service">
-            <i class="icon fa fa-globe"></i>
-            <h3 class="title">Paid Search and Social Management</h3>
-            <p class="description">Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back</p>
+        <div class="row">
+            <h2>Services</h2>
+            <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back into the development of the asset through its charitable foundation.</p>
         </div>
-        <div class="service">
-            <i class="icon fa fa-globe"></i>
-            <h3>Direct Response Content</h3>
-            <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back</p>
+        <div class="row services-list">
+            <div class="service">
+                <i class="icon fa fa-globe"></i>
+                <h3 class="title">Paid Search and Social Management</h3>
+                <p class="description">Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back</p>
+            </div>
+            <div class="service">
+                <i class="icon fa fa-globe"></i>
+                <h3>Direct Response Content</h3>
+                <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back</p>
+            </div>
+            <div class="service">
+                <i class="icon fa fa-globe"></i>
+                <h3>CRO and Retention Optimizations</h3>
+                <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back</p>
+            </div>
         </div>
-        <div class="service">
-            <i class="icon fa fa-globe"></i>
-            <h3>CRO and Retention Optimizations</h3>
-            <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back</p>
-        </div>
-    </div>
-</section>`;
+    </section>`;
 }
 
 module.exports = servicesSection;
