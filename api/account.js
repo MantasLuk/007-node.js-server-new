637f9d2ba65f9744033248741337bdd0ff2ebec0
Mantas Luk <mantasluk7@gmail.com>
2022-02-08 21:20:56 +0200

    API validation

---
+++ api/account.js
@@ -0,0 +1,7 @@
+const handler = {};
+
+handler.service = () => {
+    return 'Sveikiname, account API pasiektas';
+}
+
+export default handler;
---
+++ api/contact.js
@@ -0,0 +1,7 @@
+const handler = {};
+
+handler.service = () => {
+    return 'Sveikiname, contact API pasiektas';
+}
+
+export default handler;
