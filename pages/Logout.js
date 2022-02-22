6871af362a06e4b0ffd2fe451c1197c5f276bc32
Mantas Luk <mantasluk7@gmail.com>
2022-02-22 19:53:07 +0200

    Remove login-token cookie

---
+++ pages/Logout.js
@@ -0,0 +1,27 @@
+import { PageTemplate } from "../lib/Page.js";
+
+class PageLogout extends PageTemplate {
+    constructor(data) {
+        super(data);
+        this.isLogoutPage = true;
+        this.data.user.isLoggedIn = false;
+    }
+
+    mainHTML() {
+        return `<section class="container hero">
+                    <div class="row">
+                        <div class="left">
+                            <h1>Logout</h1>
+                            <p>You have been succesfully logged out</p>
+                            <a href="/" class="btn">Go back home</a>
+                        </div>
+                        <div class="right">
+                            <img src="/img/hero.png" alt="Hero image">
+                        </div>
+                    </div>
+                </section>`;
+    }
+}
+
+export { PageLogout };
+
